56bef306d4eefbea27958767d9e2e206f02e823a
VISTALL <vistall.valeriy@gmail.com>
2013-06-05 15:09:38 +0300

    dont need - it replaced by lombok fork

--- consulo-lombok-ext/testData/com/intellij/openapi/project/Project.java
+++
@@ -1,9 +0,0 @@
-package com.intellij.openapi.project;
-
-/**
- * @author VISTALL
- * @since 17:28/03.06.13
- */
-public class Project
-{
-}
--- consulo-lombok-ext/testData/org/jetbrains/annotations/NotNull.java
+++
@@ -1,9 +0,0 @@
-package org.jetbrains.annotations;
-
-/**
- * @author VISTALL
- * @since 18:09/03.06.13
- */
-public @interface NotNull
-{
-}

04a4893da8b17ec84953771d02e9d66e2069dffe
VISTALL <vistall.valeriy@gmail.com>
2015-08-25 23:11:14 +0300

    fix bad inherit

--- forms-compiler/src/com/intellij/uiDesigner/compiler/UIGeneratorAdapter.java
+++ forms-compiler/src/com/intellij/uiDesigner/compiler/UIGeneratorAdapter.java
@@ -1,6 +1,7 @@
 package com.intellij.uiDesigner.compiler;
 
 import org.jetbrains.org.objectweb.asm.ClassVisitor;
+import org.jetbrains.org.objectweb.asm.Opcodes;
 import org.jetbrains.org.objectweb.asm.Type;
 import org.jetbrains.org.objectweb.asm.commons.GeneratorAdapter;
 import org.jetbrains.org.objectweb.asm.commons.Method;
@@ -11,17 +12,36 @@ import org.jetbrains.org.objectweb.asm.commons.Method;
  */
 public class UIGeneratorAdapter extends GeneratorAdapter
 {
-	private static final Type ourJBUIType = Type.getType("com.intellij.util.ui.JBUI");
+	private static final Type ourJBUIType = Type.getObjectType("com/intellij/util/ui/JBUI");
 	private static final Method ourScaleIntMethod = Method.getMethod("int scale(int)");
 	private static final Method ourScaleFloatMethod = Method.getMethod("float scale(float)");
 
 	private final boolean myUseJBScaling;
 
-	public UIGeneratorAdapter(int access, Method method, String signature, Type[] exceptions, ClassVisitor cv,
+	public UIGeneratorAdapter(int access,
+			Method method,
+			String signature,
+			Type[] exceptions,
+			ClassVisitor cv,
 			boolean useJBScaling)
 	{
-		super(access, method, signature, exceptions, cv);
+		super(Opcodes.ASM5, cv.visitMethod(access, method.getName(), method.getDescriptor(), signature,
+				getInternalNames(exceptions)), access, method.getName(), method.getDescriptor());
 		myUseJBScaling = useJBScaling;
+	}
+
+	private static String[] getInternalNames(Type[] types)
+	{
+		if(types == null)
+		{
+			return null;
+		}
+		String[] names = new String[types.length];
+		for(int i = 0; i < names.length; i++)
+		{
+			names[i] = types[i].getInternalName();
+		}
+		return names;
 	}
 
 	public void pushScaled(int value)
